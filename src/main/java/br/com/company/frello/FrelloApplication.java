d275e3294b030ef5bc0828d2ffbc5ea75fcc61b6
Paulo Freitas <login.paulofreitas@hotmail.com>
2023-10-22 20:12:21 -0300

    feat: update FrelloApplication

--- src/main/java/br/com/company/frello/FrelloApplication.java
+++ src/main/java/br/com/company/frello/FrelloApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FrelloApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(FrelloApplication.class, args);
     }
