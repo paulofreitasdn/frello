5ed0ff422bdf1183a3f5672f4a1446cff54d19ef
Paulo Freitas <login.paulofreitas@hotmail.com>
2023-10-25 14:19:27 -0300

    feat: update Task

--- src/main/java/br/com/company/frello/domain/task/Task.java
+++ src/main/java/br/com/company/frello/domain/task/Task.java
@@ -1,15 +1,11 @@
 package br.com.company.frello.domain.task;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "tasks")
