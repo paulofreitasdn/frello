a46aa56df1963a6fc307929398958a592b4f971a
Paulo Freitas <login.paulofreitas@hotmail.com>
2023-10-25 14:31:10 -0300

    feat: update TaskController

--- src/main/java/br/com/company/frello/controller/TaskController.java
+++ src/main/java/br/com/company/frello/controller/TaskController.java
@@ -3,25 +3,25 @@ package br.com.company.frello.controller;
 import br.com.company.frello.domain.task.ListTask;
 import br.com.company.frello.domain.task.Task;
 import br.com.company.frello.domain.task.TaskData;
-import br.com.company.frello.domain.task.UpdateTask;
 import br.com.company.frello.repository.TaskRepository;
+import br.com.company.frello.domain.task.UpdateTask;
 
 import jakarta.validation.Valid;
 
 import org.springframework.data.domain.Page;
-
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 
 import org.springframework.transaction.annotation.Transactional;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
